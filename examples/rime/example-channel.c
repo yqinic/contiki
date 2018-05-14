a97097dd82f4f465daf4527cebed1dbc2117f9c2
yqinic <yuan.qin.9206@gmail.com>
2018-05-14 09:47:53 +0100

    20180514

--- examples/rime/example-channel.c
+++ examples/rime/example-channel.c
@@ -23,17 +23,8 @@ broadcast_sent(struct broadcast_conn *c, int status, int num_tx)
 {
   printf("status: %d\n", status);
 }
-
-static void
-unicast_recv(struct unicast_conn *c, const linkaddr_t *from)
-{
-  printf("received from %d.%d, rssi: %s\n", from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
-}
-
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv, broadcast_sent};
 static struct broadcast_conn broadcast;
-static const struct unicast_callbacks unicast_call = {unicast_recv};
-static struct unicast_conn unicast;
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
@@ -45,22 +36,17 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
 
   broadcast_open(&broadcast, 129, &broadcast_call);
 
-  unicast_open(&unicast, 143, &unicast_call);
-
   while(1) {
-
-    /* Delay 2-4 seconds */
-    //etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
-
-    leds_toggle(LEDS_RED);    
 
     etimer_set(&et, CLOCK_SECOND);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
+  
+    cc2420_set_channel(20);
     packetbuf_copyfrom("Hello", 6);
     broadcast_send(&broadcast);
+    printf("broadcast message sent, ch:%d\n", cc2420_get_channel());
 
-  }
+    }
 
   PROCESS_END();
 }
