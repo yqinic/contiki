a5a0f2ca1a3fe07553a4b415fa29a8ac26912fa5
yqinic <yuan.qin.9206@gmail.com>
2018-11-20 17:21:11 +0000

    20181120

--- examples/uiwp/project-conf.h
+++ examples/uiwp/project-conf.h
@@ -1,15 +1,14 @@
-#define CHANNEL_OFFSET 26
-
+/*------------------UIWP Protocol Related Configuration -----------------------------*/
 #define FRAME_DATA_SIZE 100
-
 #define BLOCK_SIZE_OFFSET 8
 
-#define NETSTACK_CONF_RDC     nullrdc_driver
+// Offset channel
+#define CHANNEL_OFFSET 26
 
+/*----------------RIME Configuration Related-----------------*/
+#define NETSTACK_CONF_RDC     nullrdc_driver
 // #define NETSTACK_CONF_RDC     contikimac_driver
 
 #define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 16
-
 // #define WITH_PHASE_OPTIMIZATION 1
-
 #define CSMA_CONF_MAX_FRAME_RETRIES 0
